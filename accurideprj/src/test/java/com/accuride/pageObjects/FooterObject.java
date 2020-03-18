a6b68ce9fdeb8323621cb4198601055ddaeff95f
Md-Shadab <shadab.alam@indusnet.co.in>
2020-03-18 23:56:13 +0530

    Footer section updated

--- accurideprj/src/test/java/com/accuride/pageObjects/FooterObject.java
+++ accurideprj/src/test/java/com/accuride/pageObjects/FooterObject.java
@@ -1,5 +1,7 @@
 package com.accuride.pageObjects;
 
+
+
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -52,21 +54,35 @@ public void click_footer() throws Exception {
 	js.executeScript("window.scrollBy(0,2500)");
 	Thread.sleep(5000);
 
-	WebElement lt = ldriver.findElement(By.xpath("//ul[@class='footer links']/li")); 
-	
+	WebElement lt = ldriver.findElement(By.xpath("//ul[@class='footer links']")); 
 	List<WebElement> lt1 = lt.findElements(By.tagName("a"));
+	
+	for(WebElement str : lt1) {
+		
+		//String lt3 =str.getText();
+		String lt3 = str.getAttribute("href");
+	System.out.println(str.getAttribute("href"));
+		str.click();
+		ldriver.navigate().back();
+		Thread.sleep(3000);
+	
+	}
+	//webElement lt2=lt1.size();
+	
+/*	System.out.print(lt1.get(2).getAttribute("href"));
 	//int z =footer.size();
 	int z= lt1.size();
 	
-	for(int i=0;i<z;i++) {
+	for(WebElement webEle : lt1) {
 		
-		lt1.get(i).click();
+		webEle.click();
 		ldriver.navigate().back();
 		Thread.sleep(3000);
-	}
+	}*/
 	
 	}
 }
+
 
 
 
